c71f6c4f576588356c002b8d747f160424f30a6d
Aryan Prajapati <aryankp1310@gmail.com>
2025-03-17 11:33:19 +0530

    three-biyt

--- adder.v
+++ adder.v
@@ -1,11 +1,11 @@
 module adder(
-    input [1:0] a, b,
+    input [2:0] a, b,
     input cin,
-    output [1:0] sum,
+    output [2:0] sum,
     output cout
 );
-    wire c1; // Carry between the bits
-
+    wire c1, c2; 
     adder FA0 (a[0], b[0], cin, sum[0], c1);
-    adder FA1 (a[1], b[1], c1, sum[1], cout);
+    adder FA1 (a[1], b[1], c1, sum[1], c2);
+    adder FA2 (a[2], b[2], c2, sum[2], cout);
 endmodule
