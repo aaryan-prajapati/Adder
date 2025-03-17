71483ddfc57242f4514eb39f9009a2b1017460f4
Aryan Prajapati <aryankp1310@gmail.com>
2025-03-17 11:29:17 +0530

    two-bit

--- adder.v
+++ adder.v
@@ -1,7 +1,11 @@
-module one_bit_adder(
-    input A, B, Cin,
-    output Sum, Cout
+module adder(
+    input [1:0] a, b,
+    input cin,
+    output [1:0] sum,
+    output cout
 );
-	assign Sum = A ^ B ^ Cin;
-	assign Cout = (A & B) | (B & Cin) | (A & Cin);
+    wire c1; // Carry between the bits
+
+    adder FA0 (a[0], b[0], cin, sum[0], c1);
+    adder FA1 (a[1], b[1], c1, sum[1], cout);
 endmodule
